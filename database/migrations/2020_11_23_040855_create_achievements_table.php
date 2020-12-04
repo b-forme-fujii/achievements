4f5df2041b318d0eb73860589b7f8569c9a70cdd
b-forme-fujii <fujii@b-forme.net>
2020-12-04 12:54:41 +0900

    マイグレーションファイルの編集、追加

--- database/migrations/2020_11_23_040855_create_achievements_table.php
+++ database/migrations/2020_11_23_040855_create_achievements_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAchievementTable extends Migration
+class CreateAchievementsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,34 +12,27 @@ class CreateAchievementTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->tinyInteger('school_id', 1);
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('full_name');
-            $table->integer('age');
-            $table->timestamps();
-        });
+    
         {
             Schema::create('achievements', function (Blueprint $table) {
                 $table->increments('id');
-                $table->integer('user_id')->unique();
-                $table->foreign('user_id')
-                ->references('id')
-                    ->on('users');
+                $table->integer('user_id')->unsigned()->unique();
                 $table->date('insert_date');
                 $table->string('presence_absence')->nullable();
                 $table->date('start_time');
                 $table->date('end_time')->nullable();
                 $table->string('visit_support')->nullable();
-                $table->tinyInteger('food', 1)->default(0);
-                $table->tinyInteger('outside_support', 1)->default(0);
+                $table->tinyInteger('food')->default(0);
+                $table->tinyInteger('outside_support')->default(0);
                 $table->tinyInteger('medical__support')->default(0);
                 $table->string('note')->nullable();
                 $table->string('stamp')->nullable();
                 $table->timestamps();
+
+                $table->foreign('user_id')
+                ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
             });
     }
 
@@ -50,6 +43,6 @@ class CreateAchievementTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('achievement');
+        Schema::dropIfExists('achievements');
     }
 }
