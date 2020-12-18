4c27c1d725361f4043bcb1769f0f2bfbc9459ba3
b-forme-fujii <fujii@b-forme.net>
2020-12-18 11:40:11 +0900

    カレンダー情報取得処理の作成

---
+++ .history/app/Calendar/Calendar_20201218113347.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Calendar;
+
+use Carbon\Carbon;
+
+class CalendarView {
+
+	private $carbon;
+
+	function __construct($date){
+		$this->carbon = new Carbon($date);
+	}
+	/**
+	 * タイトル
+	 */
+	public function getTitle(){
+		return $this->carbon->format('Y年n月');
+	}
