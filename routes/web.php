c59885595acea1d7e5614d20f44a026e23ee6f1e
b-forme-fujii <fujii@b-forme.net>
2021-01-11 15:06:54 +0900

    今日の日付と開始時間の登録処理のルートを作成

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,14 @@
 // });
 Route::get('hello', 'HelloController@index');
 
-
 /**ユーザー選択ページ*/
 Route::get('/', 'UserController@index');
 
 /**実績登録ページ*/
 Route::get('/achievement', 'AchievementController@selection');
+
+/**今日の開始時間と登録日を実績テーブルに作成 */
+Route::get('/insert_date', 'AchievementController@insert_date');
 
 /**ログイン必須ページ*/
 Auth::routes();
