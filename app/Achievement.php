501219f484292dd61499d5b8fadde9210e8d3265
b-forme-fujii <fujii@b-forme.net>
2021-01-11 11:49:16 +0900

    formの表示、非表示の処理変更

--- app/Achievement.php
+++ app/Achievement.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Illuminate\Http\Request;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Achievement extends Model
@@ -17,4 +19,29 @@ class Achievement extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    /**
+     * ユーザーの今日のデータを取得
+     * 
+     */
+    public function getOneRecode(Request $request)
+    {
+        $now = Carbon::now()->format("Y-m-d");
+
+        $one_recode = Achievement::where('user_id', $request->id)
+            ->wheredate('insert_date', $now)
+            ->select(
+                'achievements.user_id',
+                'achievements.insert_date',
+                'achievements.start_time',
+                'achievements.end_time',
+                'achievements.food',
+                'achievements.outside_support',
+                'achievements.medical__support',
+                'achievements.note'
+            )
+            ->first();
+            return($one_recode);
+    }
+
 }
