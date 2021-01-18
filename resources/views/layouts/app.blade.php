3a1f9cd37e864db8aeb9e3a4021167d2c4a1ed8f
b-forme-fujii <fujii@b-forme.net>
2021-01-18 14:28:03 +0900

    ログイン画面で新規登録とログインを非表示に変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    {{--  <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
@@ -69,7 +69,7 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul>
+                    </ul>  --}}
                 </div>
             </div>
         </nav>
