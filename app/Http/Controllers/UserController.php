870d2c65128e6e9869b8f49b4b6b90a0630e8708
b-forme-fujii <fujii@b-forme.net>
2021-01-28 14:13:40 +0900

    ユーザー情報の編集処理の作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,19 +11,18 @@ use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     /**
-     * ユーザー選択ページ
-     * @param Request $request
-     * @return void
+     * ユーザー選択ページ（スタートページ）
+     * 本校と2校別でユーザー情報を取得
      */
     public function index()
     {
-       //本校のユーザー情報の取得
-       $school_1 = new User();
-       $school_1 = $school_1->School_1();
+        //本校のユーザー情報の取得
+        $school_1 = new User();
+        $school_1 = $school_1->School_1();
 
-       //2校のユーザー情報の取得
-       $school_2 = new User();
-       $school_2 = $school_2->School_2();
+        //2校のユーザー情報の取得
+        $school_2 = new User();
+        $school_2 = $school_2->School_2();
 
         $data = [
             'school_1' => $school_1,
@@ -32,19 +31,25 @@ class UserController extends Controller
         return view('achievement.index', $data);
     }
 
-    //新規利用者登録ページへ移動
+    /**
+     * 新規利用者登録ページへ移動
+     */
     public function add_user()
     {
-        return view('master.add');
+        return view('master.add_user');
     }
 
-    //新規利用者の登録
+    /**
+     * 新規利用者の登録処理
+     * @param Request $request
+     * @return void
+     */
     public function create_user(Request $request)
     {
         //バリデーションの実行
         $this->validate($request, User::$rules, User::$messages);
 
-         User::create(
+        User::create(
             [
                 'first_name' => $request->first_name,
                 'last_name' => $request->last_name,
@@ -53,9 +58,75 @@ class UserController extends Controller
                 'school_id' => $request->school_id
             ],
         );
+        // 一覧表示画面へリダイレクト
+        return redirect('/master');
+    }
 
+    /**
+     * ユーザー情報編集ページの処理
+     * @param Request $request
+     * @return void
+     * requestでidが送られてきているかで処理を分岐
+     */
+    public function edit_user(Request $request)
+    {
+        //user_idがnullだった場合は在籍校別のユーザー情報のみを取得
+        if ($request->user_id == null) {
+            //本校のユーザー情報の取得
+            $school_1 = new User();
+            $school_1 = $school_1->School_1();
 
-         // 一覧表示画面へリダイレクト
-         return redirect('/master');
+            //2校のユーザー情報の取得
+            $school_2 = new User();
+            $school_2 = $school_2->School_2();
+
+            $data = [
+                'school_1' => $school_1,
+                'school_2' => $school_2,
+            ];
+            return view('master.edit_user', $data);
+        } else {
+            //本校のユーザー情報の取得
+            $school_1 = new User();
+            $school_1 = $school_1->School_1();
+
+            //2校のユーザー情報の取得
+            $school_2 = new User();
+            $school_2 = $school_2->School_2();
+
+            //ユーザー情報を取得
+            $user = new User();
+            $user = $user->getUser($request);
+
+            $data = [
+                'school_1' => $school_1,
+                'school_2' => $school_2,
+                'user' => $user,
+            ];
+            return view('master.edit_user', $data);
+        }
+    }
+
+    /**
+     * ユーザー情報を編集
+     * @param Request $request
+     * @return void
+     */
+    public function update_user(Request $request)
+    {
+        //バリデーションの実行
+        $this->validate($request, User::$rules, User::$messages);
+
+        //ユーザー情報を取得
+        $user = new User();
+        $user = $user->getUser($request);
+
+         /**formの内容を全て取得 */
+         $form = $request->all();
+         /**内容を更新して保存 */
+         $user->fill($form)->save();
+         
+          return redirect('/master');
+
     }
 }
