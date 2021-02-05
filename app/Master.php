a928cc896c0e4ce73acee088b0490c6305b7e42f
b-forme-fujii <fujii@b-forme.net>
2021-02-05 13:39:34 +0900

    利用者の実績データ取得処理を作成

--- app/Master.php
+++ app/Master.php
@@ -3,7 +3,8 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable; 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
 
 class Master extends Authenticatable
@@ -36,4 +37,58 @@ class Master extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     *　利用者一覧と当月の実績データと実績表に必要なデータを取得
+     * @pahram Request $request
+     * @return void
+     * 在籍校別で利用者情報を取得する
+     */
+    public function Records(Request $request)
+    {
+
+        //本校のユーザー情報の取得
+        $school_1 = new User();
+        $school_1 = $school_1->School_1();
+
+        //2校のユーザー情報の取得
+        $school_2 = new User();
+        $school_2 = $school_2->School_2();
+
+        //月初を取得
+        $month = new Achievement();
+        $month = $month->Beginning($request);
+
+        //当月の日数を取得
+        $days = new Achievement();
+        $days = $days->One_Month($request);
+
+        //今月から過去1年間の月を取得
+        $years = new Achievement();
+        $years = $years->One_Year();
+
+        //過去の月の実績表を取得するための引数を取得
+        $nums = new Achievement();
+        $nums = $nums->Manth_Nums();
+
+        //該当ユーザーの情報をを取得
+        $user = new User();
+        $user = $user->getUser($request);
+
+        //該当ユーザーの月の実績データの取得
+        $records = new Achievement();
+        $records = $records->Month_Records($request);
+
+        $data = [
+            'school_1' => $school_1,
+            'school_2' => $school_2,
+            'month' => $month,
+            'days' => $days,
+            'years' => $years,
+            'nums' => $nums,
+            'user' => $user,
+            'records' => $records,
+        ];
+        return ($data);
+    }
 }
