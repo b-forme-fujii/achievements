d638e54a5e1dc6b4cac2c4c4f029c525292ed873
b-forme-fujii <fujii@b-forme.net>
2021-02-19 13:14:42 +0900

    ヘッダ－にナビリンクを作成

--- resources/views/master/add_user.blade.php
+++ resources/views/master/add_user.blade.php
@@ -6,10 +6,51 @@
     <div class="col-md-8">
         <div class="card">
             <div class="card-header">
-                <div class="row">
-                    <p class="mt-3 ml-2">新規利用者登録</p>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="/master">
+                        <font class="master_title">新規利用者登録</font>
+                    </a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto"></ul>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                            @endif
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle mt-1 mx-2" href="#" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    管理者機能一覧<span class="caret"></span>
+                                </a>
 
-                </div>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item text-black" href="/master">実績閲覧</a>
+                                    <a class="dropdown-item text-black" href="/add_user">新規利用者登録</a>
+                                    <a class="dropdown-item text-black" href="/edit_user">利用者情報の編集</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </nav>
             </div>
             <div class="row justify-content-center">
                 <div class="card-body">
